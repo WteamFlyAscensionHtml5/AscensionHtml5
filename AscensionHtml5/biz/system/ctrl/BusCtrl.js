1aeb81f6db54d7ed15463452930940c95531fa1e
Evrn <Evrn@Evrn-PC>
2013-08-10 20:32:33 +0800

    完善注释

--- AscensionHtml5/biz/system/ctrl/BusCtrl.js
+++ AscensionHtml5/biz/system/ctrl/BusCtrl.js
@@ -1,7 +1,7 @@
 /**
  * 业务层入口类，中转前端反问后端.
  * 
- * @author 待填.
+ * @author 梁俊杰.
  * @version v0.20.
  */
 
@@ -15,7 +15,7 @@ if (typeof BusCtrl == 'undefined') {
  * 
  * @param command 操作命令
  * @param request 传入的参数哈希表
- * @author 待填.
+ * @author 梁俊杰.
  * @version v0.20.
  */
 BusCtrl.prototype.execute = function(command, request) {
